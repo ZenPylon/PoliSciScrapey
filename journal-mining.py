d79882a5c1cd43749c8dbe2d435dfa2fd0d7e3de
zenpylon <ajdecker1022@gmail.com>
2018-02-07 14:44:36 -0600

    more firebase'

--- journal-mining.py
+++ journal-mining.py
@@ -18,8 +18,8 @@ db = firestore.client()
 search_wait_time = 4
 results_per_page = 10
 start_index = 0
-search_offset = 130
-max_start = 140
+search_offset = 0
+max_start = 50
 querier = scholar.ScholarQuerier()
 query = scholar.SearchScholarQuery()
 
@@ -45,8 +45,9 @@ existing_query = queries \
 if sum(1 for x in existing_query ) != 0:
     print(f'query for {pub_name} in {pub_year} already run (or in progress).  Skipping...')
 else:
+    print('marking query as started in firestore')
     # Create a document to mark this query
-    queries.set({
+    query_doc_ref = queries.add({
         'completed': False,
         'created_at': int(time.time()),
         'publication_title': pub_name,
@@ -54,22 +55,38 @@ else:
         'start_index': int(start_index)
     })
 
+    total_matches = 0
+    matches_with_pdf = 0
     while search_offset < max_start:
         query.set_start(search_offset)
         querier.send_query(query)
         articles = querier.articles
-        # articles = list(filter(lambda x: x['title'] == pub_name, articles))
+
+        # If we reach the end of the results page    
+        if len(articles) == 0:
+            break
+    
+        # Fitler articles to include only those with pdf links and exact
+        # publication matches
+        articles = list(filter(lambda x: x['title'] == pub_name, articles))
         pdfs = list(filter(lambda x: x['url_pdf'] is not None, articles))
+        total_matches += len(articles)
+        matches_with_pdf += len(pdfs)
 
         print(f'Printing results from {search_offset} to {search_offset + results_per_page}')
-        print(f'number of articles is {len(articles)}')
-        print(f'filtered length is {len(pdfs)}')
         
-        for art in articles:
-            print(str(art.as_txt()) + '\n')
+        for pdf in pdfs:
+            pdf['url_pdf']
 
         time.sleep(search_wait_time)
         search_offset += results_per_page
+    
+    # Mark our query as complete, and note the number of matches / pdfs
+    query_doc_ref.update({
+        'completed': True,
+        'total_matches': total_matches,
+        'matches_with_pdf': matches_with_pdf
+    })
 
    
     
